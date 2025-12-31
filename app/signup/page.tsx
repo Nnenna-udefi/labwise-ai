ec006b51e11cd51e01d871bfbcd19a9c34413968
Nnenna udefi <miriamudefi@gmail.com>
2025-12-31 02:29:07 +0100

    added password checker

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,6 +6,7 @@ import { Input } from "@/src/ui/input";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import PasswordChecklist from "react-password-checklist";
 
 const supabase = createClient();
 
@@ -16,6 +17,7 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [passwordAgain, setPasswordAgain] = useState("");
 
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
@@ -48,7 +50,7 @@ const Signup = () => {
   }
   return (
     <div className="min-h-screen py-10 px-4">
-      <div className="bg-foreColor container rounded-2xl max-w-[500px] mx-auto px-4 md:px-6 py-8 lg:px-8  h-full flex flex-col gap-4">
+      <div className="bg-foreColor container rounded-2xl max-w-125 mx-auto px-4 md:px-6 py-8 lg:px-8  h-full flex flex-col gap-4">
         <div className="text-center py-4">
           <h1 className="text-3xl font-bold tracking-tight md:text-4xl">
             Create an Account
@@ -86,6 +88,28 @@ const Signup = () => {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">Enter Password Again:</label>
+            <Input
+              value={passwordAgain}
+              type="password"
+              placeholder="*******"
+              onChange={(e) => setPasswordAgain(e.target.value)}
+            />
+          </div>
+          <PasswordChecklist
+            rules={["minLength", "specialChar", "number", "capital", "match"]}
+            minLength={8}
+            value={password}
+            valueAgain={passwordAgain}
+            messages={{
+              minLength: "Password must be at least 8 characters long.",
+              specialChar: "Password must contain a special character.",
+              number: "Password must contain a number.",
+              capital: "Password must contain a capital letter.",
+              match: "Passwords must match.",
+            }}
+          />
           {error && <p className="text-danger">{error}</p>}
           <Button disabled={loading}>
             {loading ? "Signing up..." : "Sign Up"}
